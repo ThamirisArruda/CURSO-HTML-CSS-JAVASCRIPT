6ff25ffafaefc5c738253b9641b53f3c133d542c
thamiris_arruda <thamydyanara@gmail.com>
2022-11-22 13:11:57 -0300

    add exe

--- JAVASCRIPT/JavaScript-DIO/05-objetos/aula03.js
+++ JAVASCRIPT/JavaScript-DIO/05-objetos/aula03.js
@@ -9,24 +9,23 @@ class Pessoa {
         this.anoDeNascimento = 2022 - idade;
     }
 
-
     descrever() {
-        console.log('Meu nome é ${this.nome} e minha idade é ${this.idade}');
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
 }
-function compararPessoas(pq, p2) {
+function compararPessoas(p1, p2) {
     if (p1.idade > p2.idade) {
-        console.log('${p1.nome} é mais velho(a) que ${p2.nome}');
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
     }
     else if (p2.idade > p1.idade) {
-        console.log('${p2.nome} é mais velho(a) que ${p1.nome}');
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
     }
     else {
-        console.log('${p1.nome} e ${p2.nome} tem a mesma idade');
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
     }
 }
 
-const Thamiris = new Pessoa ('Thamiris, 23');
-const Dyanara = new Pessoa ('Dyanara, 24');
+const Thamiris = new Pessoa ('Thamiris', 23);
+const Dyanara = new Pessoa ('Dyanara', 24);
 
-compararPessoas(thamiris, dyanara);
+compararPessoas(Thamiris, Dyanara);
