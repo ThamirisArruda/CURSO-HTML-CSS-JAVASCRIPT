f686cdf4693e4109829a30506185a4e4a416d86e
thamiris_arruda <thamydyanara@gmail.com>
2023-01-02 18:44:50 -0300

    add

---
+++ JAVA/DIO-JAVA/src/BoletimEstudantil.java
@@ -0,0 +1,16 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else 
+            System.out.println("Aprovado");
+    }
+}
+
+//Uma variavel deve ser clara, sem abreviações ou definição sem sentido;
+//Uma variavel é sempre no singular, exceto quando se referir a um array ou coleção;
+//Idioma unico.
+// [] significa mais opções
---
+++ JAVA/DIO-JAVA/src/MinhaClasse.java
@@ -0,0 +1,27 @@
+public class MinhaClasse {
+    public static void main (String [] args) {
+        String primeiroNome = "Thamiris";
+        String segundoNome = "Dyanara";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
+
+//Variaveis
+// Os unicos simbolos permitidos são _ &
+//Uma variavel não pode começar com numero
+//Não pode ter espaço no nome da variavel
+//Log faz parte das palavras reservadas da lingua
+
+//TIPO      NOMEDEFINIDO  VALOR
+//boolean   verdadeira    true;
+
+//Metodos
+//TipoRetorno NomeNoInfinitivo Parametros
+//String      FormatarCep      (long cep)
